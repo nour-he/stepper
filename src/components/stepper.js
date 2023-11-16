92d0b6058bebe1980f5563371c3c98193ccc2986
nour hellani <nour.hellani@creoshift.com>
2023-11-16 14:33:06 +0200

    fixes

--- src/components/stepper.js
+++ src/components/stepper.js
@@ -1,18 +1,16 @@
 // Stepper.js
 import React from "react";
 
-const Stepper = ({ currentStep }) => {
-  const steps = ["Step 1", "Step 2"]; // Add more steps as needed
-
+const Stepper = ({ steps, currentStep }) => {
   return (
     <div className="flex justify-center mt-4">
       {steps.map((step, index) => (
         <div
           key={index}
-          className={`w-16 h-2 mx-2 ${
-            index === currentStep ? "bg-blue-500" : "bg-gray-300"
-          }`}
-        ></div>
+          className={`step ${index === currentStep ? "active" : ""}`}
+        >
+          {step}
+        </div>
       ))}
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import Step1 from "@/components/step1";
 import Step2 from "@/components/step2";
+import Step3 from "@/components/step3";
 import Stepper from "@/components/stepper";
+import SvgLogo from "@/components/svgs/logo";
+
 import React, { useState } from "react";
 
 export default function Home({}) {
   const [currentStep, setCurrentStep] = useState(0);
+  const steps = ["1", "2", "3"];
 
   const handleNext = () => {
     setCurrentStep((prevStep) => prevStep + 1);
@@ -16,10 +20,37 @@ export default function Home({}) {
 
   return (
     <div>
-      <Stepper currentStep={currentStep} />
-      {currentStep === 0 && <Step1 onNext={handleNext} />}
-      {currentStep === 1 && <Step2 onPrev={handlePrev}  />}
-      {/* Add more steps as needed */}
+      <div className="lg:hidden flex onboarding-logo">
+        <div className=" min-w-[260px]">
+          <SvgLogo />
+        </div>
+      </div>
+      <div className="h-screen bg-gray_1 w-full onboarding min-h-[600px]">
+        <div className="container mx-auto flex lg:justify-between justify-center items-center relative w-full h-full ">
+          <div className="lg:block hidden ">
+            <div className="">
+              <div className="absolute top-16  left-24 2xl:left-52 z-50 w-[360px] h-[72px]">
+                <SvgLogo />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col  mt-10  items-center text-white lg:w-[60%]">
+            <div className="mb-6">
+              <h1 className="text-28 font-clash-medium  text-center  lg:w-full w-[256px]">
+                Enter your confirmation code
+              </h1>
+              <Stepper steps={steps} currentStep={currentStep} />
+              {currentStep === 0 && <Step1 onNext={handleNext} />}
+              {currentStep === 1 && (
+                <Step2 onPrev={handlePrev} onNext={handleNext} />
+              )}
+              {currentStep === 2 && (
+                <Step3 onPrev={handlePrev} onNext={handleNext} />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
